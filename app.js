be38424c72a39abffead3ebd91ca0305e0814afe
rodg22 <giudicerodrigo@hotmail.com>
2024-02-06 19:08:28 -0300

    Se valida que no considere el .gitkeep

--- app.js
+++ app.js
@@ -16,22 +16,24 @@ const processImg = async () => {
     const files = await fse.readdir(inputFolder);
 
     for (const file of files) {
-      let inputPath = `${inputFolder}/${file}`;
-      let outputPath = `${outputFolder}/${file}`;
+      if (!file.includes(".gitkeep")) {
+        let inputPath = `${inputFolder}/${file}`;
+        let outputPath = `${outputFolder}/${file}`;
 
-      await sharp(inputPath).resize(targetWidth).toFile(outputPath);
+        await sharp(inputPath).resize(targetWidth).toFile(outputPath);
 
-      await imagemin([outputPath], {
-        destination: outputFolder,
-        plugins: [
-          imageminJpegtran({ quality: 80 }),
-          imageminPngquant(),
-          imageminSvgo(),
-          imageminWebp({ quality: 80 }),
-          imageminGifsicle(),
-        ],
-      });
-      console.log(`Se ha optimizado la imagen: ${file}`);
+        await imagemin([outputPath], {
+          destination: outputFolder,
+          plugins: [
+            imageminJpegtran({ quality: 80 }),
+            imageminPngquant(),
+            imageminSvgo(),
+            imageminWebp({ quality: 80 }),
+            imageminGifsicle(),
+          ],
+        });
+        console.log(`Se ha optimizado la imagen: ${file}`);
+      }
     }
     console.log("Se ha terminado la optimización de todas las imágenes");
   } catch (error) {
