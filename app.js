b5bbb78a5cda688f18d2baeb939e384491882b7b
rodg22 <rodrigo.giudice@tsoftlatam.com>
2024-02-06 19:45:00 -0300

    Se agrega consulta de width al usuario

--- app.js
+++ app.js
@@ -10,30 +10,33 @@ import readline from "readline";
 
 let inputFolder = "src";
 let outputFolder = "opt";
-let targetWidth = 1920;
 
-const finalizarOptimizacion = () => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+const askTargetWidth = () => {
+  return new Promise((resolve, reject) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    console.log("Bienvenido al optimizador de imagenes");
+    rl.question(
+      "Ingresa el ancho deseado de las imagenes (solo numeros)\nPor ejemplo: Si digitas 1920 correspondera a un width de 1920px:\n",
+      (width) => {
+        rl.close();
+        resolve(parseInt(width));
+      }
+    );
   });
-  console.log("La optimización de imagenes ha finalizado");
-  // Imprime un mensaje y espera la entrada del usuario
-  rl.question("Presiona cualquier tecla para finalizar...", (answer) => {
-    // Cuando el usuario ingresa una tecla, cierra la interfaz readline
-    rl.close();
-  });
+};
 
-  // Maneja el evento 'close' de la interfaz readline
-  rl.on("close", () => {
-    console.log("Saliendo...");
-    // Coloca aquí cualquier código que desees ejecutar antes de salir
-    process.exit(0); // Sale del proceso con código de éxito
-  });
+const mensajeParaFinalizarOptimizacion = () => {
+  console.log(
+    "La optimización de imagenes ha finalizado\nLas encontraras en la carpeta opt"
+  );
 };
 
 const processImg = async () => {
   try {
+    const targetWidth = await askTargetWidth();
     const files = await fse.readdir(inputFolder);
 
     for (const file of files) {
@@ -56,7 +59,7 @@ const processImg = async () => {
         console.log(`Se ha optimizado la imagen: ${file}`);
       }
     }
-    finalizarOptimizacion();
+    mensajeParaFinalizarOptimizacion();
   } catch (error) {
     console.error(error);
   }
